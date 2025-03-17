7ba052a99c75df3d364fd521466c7f1e6635ed8a
shujanshaikh <shaikhshujan@gmail.com>
2025-03-17 07:30:01 +0530

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 
 import { currentUser } from "@clerk/nextjs/server"
-import LegalWebsite from "./components/dashboard"
+import LegalWebsite from "./components/Dashboard"
 
 export default async function Home() {
   const user = await currentUser()

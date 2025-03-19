eb64a93a94d130482eddfc5709c9eaf25a812976
samar sayed <sayedsamar58@gmail.com>
2025-03-19 21:57:06 +0530

    Removed ui/ folder and dashboard file

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,30 +1,8 @@
-
-<<<<<<< HEAD
-
 import { redirect } from "next/navigation"
-
-
-
 
 
 export default async function Home() {
 
 redirect ("/home")
  
-=======
-import { currentUser } from "@clerk/nextjs/server"
-import LegalWebsite from "./components/Dashboard"
-
-export default async function Home() {
-  const user = await currentUser()
-
-  if (!user) return <div> <LegalWebsite /></div>
-
-  return (
-    <div >
-       Main Page
-      
-    </div>
-  )
->>>>>>> upstream/main
 }
